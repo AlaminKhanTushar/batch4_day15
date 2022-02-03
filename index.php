eaef03ce346be91f07d32a3fdb26d8bbfc89cbd5
Tanveerbitm <cityuniversity303@gmail.com>
2022-02-03 17:24:33 +0600

    PHP-Day2

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'vendor/autoload.php';
+use App\classes\Example;
+
+$example = new Example();
+$example->index();
+
+/*
+ * *3 major rules for variable
+        * start with $ sign
+        * A-Z, a-z, 0-9, _ ()
+        * no number in first
+ *
+ *
+ * * stander rules for variable naming
+        * start with small letter
+        * meaningful
+        * readable
+
+ */
+
+//$1ToGo = 10;
+//$FirstNumber = 10;
+//$lastnumber = 10;
+//$firstName = 10;
